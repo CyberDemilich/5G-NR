2ed5bc52841e73ceab1dc6f3d76dda43f491e4af
CyberDemilich <59976579+CyberDemilich@users.noreply.github.com>
2022-09-30 17:16:39 +0000

    new code

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-class gNB:
-  pass
+class Core5G():
+  def __init__(self):
+    self.amfs = [AMF(1), AMF(2)]
+    self.upfs = [UPF(1), UPF(2)]
+    self.gnbs = [gNB(1), gNB(2)]
+
+class AMF():
+  def __init__(self, id: int):
+    self.id = id
+
+class UPF():
+  def __init__(self, id: int):
+    self.id = id
+
+class gNB():
+  def __init__(self, id: int):
+    self.id = id
 
 if __name__ == "__main__":
-  gnb = gNB()
+  core = Core5G()
+  print(core)
